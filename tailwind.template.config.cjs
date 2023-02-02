eafd83435ee2129bb2cf1e484985d13c49bd007a
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2023-02-02 11:54:53 -0700

    fix: animate-pulse

--- tailwind.template.config.cjs
+++ tailwind.template.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/assets/template.html"],
+  content: ["./src/assets/template.{html,js}"],
   theme: {
     extend: {},
   },
